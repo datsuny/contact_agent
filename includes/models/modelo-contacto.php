628ff78930bd523bed745c618deff9a6b4ad1efb
David Corrales <david_519cs@hotmail.com>
2021-05-07 21:39:27 -0600

    insert model created without save the data

--- includes/models/modelo-contacto.php
+++ includes/models/modelo-contacto.php
@@ -1,2 +1,33 @@
 <?php
 
+if($_POST['accion'] === 'crear'){
+    //crear un nuevo registro
+    require_once('../functions/database.php');
+
+    //validar las entradas para evitar SQL injections
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+    $telefono2 = filter_var($_POST['telefono2'], FILTER_SANITIZE_STRING);
+
+    try {
+        $statement = $conn->prepare("INSERT INTO contacto (nombre, email, telefono, telefono2) VALUES (?,?,?,?)");
+        $statement->bind_param("ssss", $nombre,$email,$telefono,$telefono2);
+        $statement->execute();
+
+        $respuesta = array(
+            'respuesta' => 'correcto',
+            'info' => $statement
+        );
+
+        $statement->close();
+        $conn->close();
+
+    } catch (Exception $e) {
+        $respuesta = array(
+            'error' => $e.getMessage()
+        );
+    }
+
+    echo json_encode($respuesta);
+}
